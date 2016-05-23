95bc32b98b70912cd52432e6ee7bf2193d51b69b
GHackAnonymous <ederaraia@gmail.com>
2016-05-23 19:54:51 +0200

    Clase para crear Objetos de tipo Cuenta.
    Esta clase contiene 5 variables 4 de tipo String y 1 de tipo int.
    
    Contructor sin y con parametro para poder crear vacios o cargar 
    datos cuando lo obtienes de la base de datos.
    
    A parte tienen las variables sus propios get y set para poder 
    cojer los datos y introducir los datos.

---
+++ src/llaverocuentasinternet/Cuenta.java
@@ -0,0 +1,68 @@
+package llaverocuentasinternet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author GHackAnonymous
+ */
+public class Cuenta {
+    private int id = 0;
+    private String web = "";
+    private String usuario = "";
+    private String correo = "";
+    private String clave = "";
+    
+    public Cuenta(){}
+    public Cuenta(int id, String web, String usuario, String correo, String clave){
+        this.id = id;
+        this.web = web;
+        this.usuario = usuario;
+        this.correo = correo;
+        this.clave = clave;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+}
